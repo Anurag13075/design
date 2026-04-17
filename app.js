333a92379dac891d47cfe57d5cba03d3761e0e9b
anurag <yourboyanurag777@gmail.com>
2026-04-17 15:05:34 +0530

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 import express from "express";
-const app = express();
 import { connect } from "./db/db.js";
-import dotenv from "dotenv"
-import cors from "cors"
-dotenv.config();
-import { googleauth } from "./auth/googleauth.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
+
+import googleRoutes from "./routes/googleroute.js";
+import designRoutes from "./routes/designroute.js";
+
+const app = express();
 
 app.use(express.json());
-
 app.use(cors());
 
-app.use('/google', googleauth)
-connect()
+app.use('/google', googleRoutes);
+app.use('/api/designs', designRoutes);
 
-app.listen(3000)
+connect();
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
