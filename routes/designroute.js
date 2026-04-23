544da5b07c69dba702681a329f4ca5fea21d410d
anurag <yourboyanurag777@gmail.com>
2026-04-23 20:59:30 +0530

    desiign routes

---
+++ routes/designroute.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+    generateDesign,
+    createDesign,
+    getAllDesigns,
+    getDesignById,
+    updateDesign,
+    deleteDesign
+} from "../controllers/designcontroller.js";
+
+const router = Router();
+
+router.post("/generate", generateDesign);
+router.post("/", createDesign);
+router.get("/", getAllDesigns);
+router.get("/:id", getDesignById);
+router.put("/:id", updateDesign);
+router.delete("/:id", deleteDesign);
+
+export default router;
