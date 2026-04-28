8b3a840bf03d7343ebe15b85a0eba938e762b4d3
anurag <yourboyanurag777@gmail.com>
2026-04-28 15:20:36 +0530

    open ai integration

--- ai/openai-integrate.js
+++ ai/openai-integrate.js
@@ -1,5 +1,28 @@
 import {MAIN_DESIGN_PROMPT} from "../utils/mainPrompt.js"
+import { OpenAI  } from "openai/client.js"
 
+const openai = new OpenAI({
+    apiKey:process.env.API_KEY
+})
+
+
+export async function  generatedesign(params) {
+    const response  = await openai.responses.create({
+        model:"gpt-4.1-mini",
+        messages:[
+            {
+                role:"user",
+                content:MAIN_DESIGN_PROMPT,
+                
+            },
+         
+            
+        ]
+    })
+    console.log(response.output_text);
+    console.log(response.max_output_tokens);
+
+}
 
 
     
